d7a7e7b7afadd8b32ed6580b9cc03bd316dbf926
alexanderxp <alexanderxp@ukr.net>
2019-07-06 21:56:35 +0300

    add backend and form controls

--- src/js/index.js
+++ src/js/index.js
@@ -26,3 +26,131 @@ const o = {
 
 console.log(o?.foo?.bar?.baz ?? 'default');
 */
+void function () {
+    var blurVal = 40;
+
+    document.addEventListener( "DOMContentLoaded", function(){
+
+        document.querySelector('body > div.container.clearfix').style.filter = 'blur('+blurVal+'px)';
+
+        var requestUsers = new XMLHttpRequest(), requestMessages = new XMLHttpRequest();
+        requestUsers.open('GET', 'https://studentschat.herokuapp.com/users', true);
+        requestMessages.open('GET', 'https://studentschat.herokuapp.com/messages', true);
+
+        requestUsers.onload = function() {
+            if (requestUsers.status >= 200 && requestUsers.status < 400) {
+            // Обработчик успещного ответа
+            var response = requestUsers.responseText;
+            var userList = Response;
+            
+            JSON.parse(response).forEach(
+                function (user, i) {
+                    var ulDomElement = document.getElementById('users-list');
+                    var liDomElement = document.createElement('li');
+                    liDomElement.innerHTML = ' ' +
+                    '<li class="clearfix">' +
+                    // '<img src="images/0' + Number(i+1) + '.png" alt="avatar">' +
+                        '<div class="about">' +
+                        '<div class="name">' + user.username + '</div>' +
+                        '<div class="status">' +
+                            '<i class="fa fa-circle online"></i> online 20 минут'
+                        '</div>' +
+                        '</div>' +
+                    '</li>';
+                    ulDomElement.appendChild(liDomElement);
+                }
+            )
+            } else {
+            // Обработчик ответа в случае ошибки
+            }
+        };
+        requestUsers.onerror = function() {
+            // Обработчик ответа в случае неудачного соеденения
+        };
+        requestUsers.send();
+
+        requestMessages.onload = function() {
+            if (requestMessages.status >= 200 && requestMessages.status < 400) {
+            // Обработчик успещного ответа
+            var response = requestMessages.responseText;
+            var messagesList = Response;
+            
+            JSON.parse(response).forEach(
+                function (user, i) {
+                    var divChatWithElement = document.getElementsByClassName('chat-with')[0];
+                    divChatWithElement.innerText = user.chatroom_id;
+                }
+            )
+            } else {
+            // Обработчик ответа в случае ошибки
+            }
+        };
+        requestMessages.onerror = function() {
+            // Обработчик ответа в случае неудачного соеденения
+        };
+        requestMessages.send();
+
+        document.querySelector('body > div.login-modal > button').addEventListener('click', function () {
+            var requestLogin = new XMLHttpRequest();
+            requestLogin.open('POST', 'https://studentschat.herokuapp.com/users/register', true);
+
+            var userName = document.querySelector('body > div.login-modal > div:nth-child(2) > input').value;
+
+            requestLogin.onload = function() {
+            // Обработчик ответа в случае удачного соеденения
+            };
+
+            requestLogin.onerror = function() {
+            // Обработчик ответа в случае неудачного соеденения
+                //document.querySelector('body > div.container.clearfix').style.visibility = "visible";
+                var i = blurVal;
+                document.querySelector('body > div.login-modal').style.visibility = "hidden";
+                var blurEffectInterval = setInterval(function () {
+                    if (!i) {
+                        clearInterval(blurEffectInterval);
+                    }
+                    document.querySelector('body > div.container.clearfix').style.filter = 'blur('+i--+'px)';                    
+                }, 50);
+            };
+            requestLogin.setRequestHeader('Content-Type', 'application/json');
+
+            requestLogin.send(JSON.stringify(userName));
+        });
+    });
+
+    window.onload = addListeners();
+    var offX;
+    var offY;
+    function addListeners() {
+        document.querySelector('.login-modal').addEventListener('mousedown', mouseDown, false);
+        window.addEventListener('mouseup', mouseUp, false);
+    }
+
+    function mouseUp () {
+    window.removeEventListener('mousemove', loginframeMove, true);
+    }
+
+    function mouseDown(e) {
+        
+        var loginDiv = document.querySelector('.login-modal');
+        offX =  e.offsetX;
+        offY =  e.offsetY;
+        window.addEventListener('mousemove', loginframeMove, true);
+    }
+
+    function loginframeMove(e) {
+        var loginDiv = document.querySelector('.login-modal');
+        loginDiv.style.position = 'absolute';
+        loginDiv.style.top = (e.clientY - offY) + 'px';
+        loginDiv.style.left = (e.clientX - offX) + 'px';
+    }
+
+    var closeBtn = document.querySelector('.close-sign');
+
+    closeBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector('.login-modal').style.display =  'none';
+    });
+
+    document.querySelector('.to-display').addEventListener('click', toggleDisplay);
+}();
